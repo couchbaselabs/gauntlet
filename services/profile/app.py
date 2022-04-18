865893f78120fa2937db3c275529cda44c10d74c
Ashwin <ashwin.trojan@gmail.com>
2022-04-18 15:23:34 +0530

    Registering deleteUser endpoint under profiles

--- services/profile/app.py
+++ services/profile/app.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from flask_restful import Api
 import logging
-from services.profile.endpoints.booking import ConfirmBooking, AllBookings, CreateUser, CancelBooking, GetBooking, EditBooking, UserAuth
+from services.profile.endpoints.booking import ConfirmBooking, AllBookings, \
+    CreateUser, CancelBooking, DeleteUser, EditBooking, GetBooking, UserAuth
 from services.profile.utils.cb_util import CBConnection
 from services.profile.utils.common_util import CommonUtil
 from services.profile.utils.defaults import Default
@@ -18,13 +19,22 @@ class App:
         self.cb = CBConnection(Default.cb_username, Default.cb_password, Default.cb_host)
         self.common_util = CommonUtil(self.cb)
 
-        self.api.add_resource(UserAuth, '/user_auth', resource_class_args=(self.cb,self.common_util))
-        self.api.add_resource(ConfirmBooking, '/confirmBooking', resource_class_args=(self.cb,self.common_util))
-        self.api.add_resource(AllBookings, '/allBookings', resource_class_args=(self.cb,self.common_util))
-        self.api.add_resource(GetBooking, '/getBooking', resource_class_args=(self.cb,self.common_util))
-        self.api.add_resource(CreateUser, '/createUser', resource_class_args=(self.cb,self.common_util))
-        self.api.add_resource(CancelBooking, '/cancelBooking', resource_class_args=(self.cb,self.common_util))
-        self.api.add_resource(EditBooking, '/editBooking', resource_class_args=(self.cb,self.common_util))
+        self.api.add_resource(UserAuth, '/user_auth',
+                              resource_class_args=(self.cb, self.common_util))
+        self.api.add_resource(ConfirmBooking, '/confirmBooking',
+                              resource_class_args=(self.cb, self.common_util))
+        self.api.add_resource(AllBookings, '/allBookings',
+                              resource_class_args=(self.cb, self.common_util))
+        self.api.add_resource(GetBooking, '/getBooking',
+                              resource_class_args=(self.cb, self.common_util))
+        self.api.add_resource(CreateUser, '/createUser',
+                              resource_class_args=(self.cb, self.common_util))
+        self.api.add_resource(DeleteUser, '/deleteUser',
+                              resource_class_args=(self.cb, self.common_util))
+        self.api.add_resource(CancelBooking, '/cancelBooking',
+                              resource_class_args=(self.cb, self.common_util))
+        self.api.add_resource(EditBooking, '/editBooking',
+                              resource_class_args=(self.cb, self.common_util))
 
     def run(self):
         self.app.run(host='0.0.0.0', debug=True)
